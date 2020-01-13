1c135344763b67c96b364ec1f755ea0882dfb23b
Joshmatjjen <joshmatparrot@gmail.com>
2020-01-13 18:12:53 +0100

    first commit

--- src/components/welcome/template.js
+++ src/components/welcome/template.js
@@ -9,11 +9,8 @@ const template = (context) => {
       <div class="card-text">
         <p>Created to help patients decide whether their symptoms require a consultation with a doctor or other health professional or can be dealt with by self-care</p>
         <p>Symptom checkers can not only be the beneficial tools for doctors but also provide the access to health care in low resource settings such as in rural areas or developing countries.</p>
-        <!--<p>We created this example to help you work with our API, and show you how to implement a simple symptom checker &mdash; an application that mimics doctor's interview and gives you a preliminary diagnosis basing on our mathematical model.</p>
-        <p>Visit our <i class="fa fa-github"></i> <a href="https://github.com/infermedica/js-symptom-checker-example">GitHub page</a> to grab the source code of this example as well as a guide on how to start working with it.</p> -->
-        <p>Checkout our symptom check <a href="https://developer.infermedica.com/docs/api">GitHub Page</a>.</p>
-        <!--<p>Please click <span class="badge badge-primary">Next</span> to move to the first question.</p>
-        <p class="text-muted small"><i class="fa fa-info-circle"></i> This is the welcome screen. In a real life scenario you will probably want to display T&C here. In the background, the new <a target="_blank" href="https://developer.infermedica.com/docs/diagnosis#helping-us-improve-the-engine">Interview-Id</a> was created.</p> -->
+        <p>Visit our <i class="fa fa-github"></i> <a href="https://github.com/Joshmatjjen/JoDs">GitHub page</a> to grab the source code of this example as well as a guide on how to start working with it.</p>
+        <p>Please click <span class="badge badge-primary">Next</span> to move to the first question.</p>
       </div>
     `);
   });
